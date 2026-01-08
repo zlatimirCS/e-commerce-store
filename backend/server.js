91395185b0f59a926df1e228a11d48b5ac4bf069
ZlatimirRamac-CS <zlatimir.ramac@concordsoft.solutions>
2026-01-08 16:08:59 +0100

    initial setup and npm installations

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
